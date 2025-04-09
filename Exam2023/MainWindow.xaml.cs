bcfdf98ed8a363f0c8315ec8f46f8d2fee8ed02a
Aymeric Leclerre Lemoine <S00277347@atu.ie>
2025-04-09 12:04:57 +0100

    aaaaa

--- Exam2023/MainWindow.xaml.cs
+++ Exam2023/MainWindow.xaml.cs
@@ -1,28 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MyMovieBookingApp.Models;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Exam2023
+namespace MyMovieBookingApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            LoadMovies();
+        }
+
+        private void LoadMovies()
+        {
+            using (var db = new MovieData())
+            {
+                var movieTitles = db.Movie
+                                    .OrderBy(m => m.Title)
+                                    .Select(m => m.Title)
+                                    .ToList();
+
+                MovieListBox.ItemsSource = movieTitles;
+            }
+        }
+
+        private void InitDb_Click(object sender, RoutedEventArgs e)
+        {
+            TestDatabase.CreateAndSeed();
+            MessageBox.Show("Base de données initialisée !");
+            LoadMovies();
+        }
+    }
+
+    public class TestDatabase
+    {
+        public static void CreateAndSeed()
+        {
+            using (var db = new MovieData())
+            {
+                if (!db.Movie.Any())
+                {
+                    var movie1 = new Movie
+                    {
+                        Title = "Inception",
+                        ImageName = "inception.jpg",
+                        Description = "A mind-bending thriller",
+                        Cast = "Leonardo DiCaprio, Joseph Gordon-Levitt"
+                    };
+
+                    var movie2 = new Movie
+                    {
+                        Title = "Interstellar",
+                        ImageName = "interstellar.jpg",
+                        Description = "A journey through space and time",
+                        Cast = "Matthew McConaughey, Anne Hathaway"
+                    };
+
+                    var booking1 = new Booking
+                    {
+                        BookingDate = DateTime.Now,
+                        NumberOfTickets = 2,
+                        Movie = movie1
+                    };
+
+                    var booking2 = new Booking
+                    {
+                        BookingDate = DateTime.Now.AddDays(1),
+                        NumberOfTickets = 4,
+                        Movie = movie2
+                    };
+
+                    db.Movie.Add(movie1);
+                    db.Movie.Add(movie2);
+                    db.Booking.Add(booking1);
+                    db.Booking.Add(booking2);
+                    db.SaveChanges();
+                }
+            }
         }
     }
 }
---
+++ Exam2023/Movie.cs
@@ -0,0 +1,33 @@
+﻿using MyMovieBookingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace MyMovieBookingApp.Models
+{
+    public class Booking
+    {
+        public int BookingID { get; set; }
+        public DateTime BookingDate { get; set; }
+        public int NumberOfTickets { get; set; }
+        public int MovieID { get; set; }
+        public Movie Movie { get; set; }
+    }
+    public class Movie
+    {
+        public int MovieID { get; set; }
+        public string Title { get; set; }
+        public string ImageName { get; set; }
+        public string Description { get; set; }
+        public string Cast { get; set; }
+        public List<Booking> Bookings { get; set; } = new List<Booking>();
+    }
+
+    public class MovieData : DbContext
+    {
+        public MovieData() : base("ODD_Exam Aymeric Leclerre") { }
+        public DbSet<Movie> Movie { get; set; }
+        public DbSet<Booking> Booking { get; set; }
+    }
+}
+
